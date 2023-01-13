ed3cedf52c77c7e78777551bdb641c769b648d92
ZOK <48816919+123ZOKzok@users.noreply.github.com>
2023-01-13 09:34:55 +0300

    Create sessions_controller.rb

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+    def create
+        user = User.find_by(fullname: params[:fullname])
+        if user&.authenticate(params[:password])
+          session[:user_id] = user.id
+          render json: user
+        else
+          render json: {errors: ["Fullname or Password Incorrect"]}, status: :unauthorized
+        end
+      end
+    
+      def destroy
+        if session[:user_id] 
+          session.destroy
+          head :no_content
+        else
+          render json: {errors: ["No user logged in"]}, status: :unauthorized
+        end
+      end
+end
